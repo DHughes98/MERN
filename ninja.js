2f5fefe43882d96643dac4e4a1dad857c2823a20
Dillon Hughes <dillonhughes29@gmail.com>
2022-10-25 17:26:33 -0400

    update

--- ninja.js
+++ ninja.js
@@ -1,27 +1,27 @@
 class Ninja {
-    constructor(name, health = 3, speed = 3, strength = 3){
-      this.name = name
-      this.health = health
-      this.speed = speed
-      this.strength = strength
+    constructor(name, health = 3, speed = 3, strength = 3) {
+        this.name = name
+        this.health = health
+        this.speed = speed
+        this.strength = strength
     }
-  
-    sayName(){
-      console.log(`My name is ${this.name}`)
+
+    sayName() {
+        console.log(`My name is ${this.name}`)
     }
-  
-    showStats(){
-      console.log(this)
+
+    showStats() {
+        console.log(this)
     }
-  
-    drinkSake(){
-      console.log("Drinking sake!")
-      this.health += 10
-      console.log("Health up! HP:", this.health)
+
+    drinkSake() {
+        console.log("Drinking sake!")
+        this.health += 10
+        console.log("Health up! HP:", this.health)
     }
-  }
-  
-  const ninja1 = new Ninja("Hidori");
-  ninja1.sayName();
-  ninja1.showStats();
-  ninja1.drinkSake();
+}
+
+const ninja1 = new Ninja("Hidori");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
